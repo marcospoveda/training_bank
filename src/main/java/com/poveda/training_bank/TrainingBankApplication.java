a93964643130abeda1af7037d61aacdaf0802e34
Marcos Poveda <marcospoveda@live.com>
2018-11-25 04:54:05 -0200

    Versão inicial do projeto.

---
+++ src/main/java/com/poveda/training_bank/TrainingBankApplication.java
@@ -0,0 +1,12 @@
+package com.poveda.training_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingBankApplication.class, args);
+	}
+}
---
+++ src/test/java/com/poveda/training_bank/TrainingBankApplicationTests.java
@@ -0,0 +1,16 @@
+package com.poveda.training_bank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrainingBankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
